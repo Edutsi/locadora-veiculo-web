7d2706fd6154c31f1bb19d114352388678504e69
edutsi <nilson.upis@gmail.com>
2020-12-14 22:12:28 -0300

    implementado cadastro de funcionario CRUD completo e aula v5.7. Adicionando mais uma enumeração - a enumeracao foi utilizada para adicionar o sexo para o  funcionario

---
+++ src/main/java/com/algaworks/curso/jpa2/modelo/Pessoa.java
@@ -0,0 +1,66 @@
+package com.algaworks.curso.jpa2.modelo;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.DiscriminatorType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Inheritance(strategy=InheritanceType.SINGLE_TABLE)//herança - tabela única o hibernate trabalha cria/pesquisa
+///evitar usar herança- pq o BD não trabalha com herança e dificulta
+@DiscriminatorColumn(name="TIPO_PESSOA", discriminatorType=DiscriminatorType.STRING)//diz qual coluna descrimina as filhas
+public class Pessoa {
+	
+	private Long codigo;
+	private String nome;
+	private Date dataNascimento;
+	private String cpf;
+	private Sexo sexo;
+	
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	public Long getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	@Temporal(TemporalType.DATE)
+	@Column(name="data_nascimento")//no banco de dados separa por _ e não pelo Case como no java
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public Sexo getSexo() {
+		return sexo;
+	}
+	public void setSexo(Sexo sexo) {
+		this.sexo = sexo;
+	}
+
+}
